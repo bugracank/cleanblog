e4504b439bfb9d168501d01dbb509b91976dc1f5
Buğracan Kaya <bcankaya@outlook.com.tr>
2023-02-24 17:35:47 +0300

    second commit ejs etc.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
-const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send(blog);
+  // res.sendFile(path.resolve(__dirname, './views/index.ejs'));
+  res.render('index.ejs');
+});
+app.get('/addphoto', function (req, res) {
+  // res.sendFile(path.resolve(__dirname, './views/index.ejs'));
+  res.render('addPhoto.ejs');
 });
 
 app.listen(3000);
