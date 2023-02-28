145268a3f7b684865014a03c00e34c3149a63612
Buğracan Kaya <bcankaya@outlook.com.tr>
2023-02-28 21:43:15 +0300

    adding cards with db

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bb-test-db');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.render('index.ejs');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index.ejs', { photos });
 });
+
 app.get('/addphoto', function (req, res) {
   res.render('addPhoto.ejs');
 });
-app.post('/photos', function (req, res) {
-  console.log(req.body);
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
   res.redirect('/');
 });
 
--- models/Photo.js
+++ models/Photo.js
@@ -2,20 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 mongoose.set('strictQuery', false);
 
-mongoose.connect('mongodb://127.0.0.1:27017/bb-test-db');
-
 const PhotoSchema = new Schema({
   title: String,
   description: String,
+  image: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Photo = mongoose.model('Photo', PhotoSchema);
 
-// Photo.create({
-//   title: 'Photo Title 2',
-//   description: 'Photo description 2',
-// });
-
-Photo.find({}, (err, data) => {
-  console.log(data);
-});
+module.exports = Photo;
