e57d46a0790d360082992344f281258c58d960e3
Buğracan Kaya <bcankaya@outlook.com.tr>
2023-02-28 21:03:25 +0300

    commit 3

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', function (req, res) {
-  // res.sendFile(path.resolve(__dirname, './views/index.ejs'));
   res.render('index.ejs');
 });
 app.get('/addphoto', function (req, res) {
-  // res.sendFile(path.resolve(__dirname, './views/index.ejs'));
   res.render('addPhoto.ejs');
+});
+app.post('/photos', function (req, res) {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.listen(3000);
